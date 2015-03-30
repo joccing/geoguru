1bee400714f83284faf1c27d2ec903692e257936
Tay Joc Cing <joccing@gmail.com>
2015-03-30 13:50:23 +0800

    Added cmain.py and data file - countries

---
+++ cmain.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3.4
+
+from classes.country import Country
+from classes.dictionary import createDict
+
+def main():
+
+    from optparse import OptionParser
+    usage = "usage: %prog [option] countries"
+    version = "0.1"
+
+    parser = OptionParser( usage = usage, version="%prog " +version )
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Make lots of noise")
+    parser.add_option("-f", "--file", action="store", dest="filename", help="Initialize shelve for country stats")
+    (options, args) = parser.parse_args()
+
+    if len(args) < 1 and options.filename == None:
+        parser.error("Invalid number of arguments")
+
+    if options.filename != None:
+        cdict = createDict( options.filename, options.verbose )
+
+        if len(args) >= 1:
+
+            # Process country queries
+            if options.verbose == True:
+                print("Retrieving information about " + args[0] + "...") 
+            
+            if args[0] in cdict:
+
+                country = cdict[args[0]]
+                stats = country.getPropertyApprox(r'.*')
+                print("%s:" % args[0] )
+                print(stats)
+
+            else:
+                print("No such country found!")
+
+if __name__ == "__main__":
+    main()
